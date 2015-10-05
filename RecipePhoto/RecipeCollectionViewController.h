d568762ed4f9f9c01bf40eedd1f8324acd163d2a
sathish chinniah <sathish73ec@gmail.com>
2015-10-05 11:51:19 +0530

    init

---
+++ RecipePhoto/RecipeCollectionViewController.h
@@ -0,0 +1,12 @@
+//
+//  RecipeCollectionViewController.h
+//  RecipePhoto
+//
+//  Created by Sathish Chinniah on 02/09/15.
+//  Copyright (c) 2015 Sathish Chinniah. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface RecipeCollectionViewController : UICollectionViewController
+
+@end
---
+++ RecipePhoto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RecipePhoto
+//
+//  Created by Sathish Chinniah on 13/09/15.
+//  Copyright (c) 2015 Sathish Chinniah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
